2047dd9daeb0791fe8776b93975b70bef92d0f47
herrsch <jack0088@me.com>
2020-09-27 14:01:08 +0200

    safty push as i havilly experiment and rewrite funcs that alter remote origin source

--- iframe.js
+++ iframe.js
@@ -6,9 +6,8 @@ function assert(condition, message) {
 
 
 
-function copy(origin, use) {
-    var api = "https://api.allorigins.win/get?url=" // https://github.com/gnuns/allorigins
-    var uri = api + encodeURIComponent(origin)
+function copy(api, origin, use) {
+    var uri = api + encodeURIComponent(origin) // https://github.com/gnuns/allorigins
     fetch(uri)
     .then(function(response) {
         if(response.ok) {
@@ -17,6 +16,60 @@ function copy(origin, use) {
     })
     .then(function(data) {
         use(data.contents)
+    })
+}
+
+
+
+function proxy(origin, use) {
+    /*
+    var reset = function(path) {
+        var host = new URL(origin)
+        var link = path // convert absolute to relative
+            .replace(host.origin, "")
+            .replace("//" + host.hostname, "")
+        if(!link.startsWith("#") // self-reference link without page reloading
+        && !link.startsWith("//") // http(s): anchronym
+        && !link.startsWith("http") // including http: and https:
+        && !link.startsWith("mailto:")
+        && !link.startsWith("callto:")
+        && !link.startsWith("tel:")
+        && !link.startsWith("fax:")
+        && !link.startsWith("maps:")
+        && !link.startsWith("geo:")
+        && !link.startsWith("fb:")
+        && !link.startsWith("twitter:")) {
+            var lol = "https://api.allorigins.win/raw?url=" + host.origin + link // convert relative to absolute with proxy prefix
+            // var lol = "https://api.allorigins.win/get?callback=use&url=" + host.origin + link // convert relative to absolute with proxy prefix
+            console.log(path, link, lol)
+            return lol
+        }
+        return path
+        // return "https://api.allorigins.win/raw?url=" + new URL(path, host.origin)
+    }
+    */
+    var host = new URL(origin)
+    var url = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig
+    function reset(query) {
+        return query.replace(url, function(path) {
+            return "https://api.allorigins.win/raw?url=" + new URL(path, host.origin)
+        })
+    }
+    copy("https://api.allorigins.win/get?url=", origin, function(source) {
+        use(reset(source))
+        /*
+        var parser = new DOMParser()
+        var doc = parser.parseFromString(source, "text/html")
+        var blacklist = doc.querySelectorAll("[src], [href], [action]")
+        for(var id = 0; id < blacklist.length; id++) {
+            var elem = blacklist[id]
+            if(elem.getAttribute("src")) elem.setAttribute("src", reset(elem.getAttribute("src")))
+            if(elem.getAttribute("href")) elem.setAttribute("href", reset(elem.getAttribute("href")))
+            if(elem.getAttribute("action")) elem.setAttribute("action", reset(elem.getAttribute("action")))
+            // url() in stylesheets?
+        }
+        use(doc.documentElement.outerHTML)
+        */
     })
 }
 
@@ -55,13 +108,6 @@ function fit(element) {
 
 
 
-function proxy(origin, source) {
-    // TODO first remove location.origin prefix from all src, href and action attributes (to make absolute paths relative again)
-    // TODO then add <base> with proxy prefix and location.origin (so that relative paths become absolute again and get routed through the proxy server)
-}
-
-
-
 function observe(element, handler) {
     var listener = new MutationObserver(handler)
     listener.observe(element, {
@@ -77,16 +123,16 @@ function observe(element, handler) {
 
 function iframe(origin, parent) {
     var element = document.createElement("iframe")
+    element.src = "about:blank"
 
     element.addEventListener("load", function() {
         var doc = element.contentDocument || element.contentWindow.document
-        proxy(origin, doc)
         observe(doc.body, fit.bind(element))
         fit(element)
     })
 
     if(origin.startsWith("http") && !origin.startsWith(window.location.origin)) {
-        copy(origin, function(source) {
+        proxy(origin, function(source) {
             element.srcdoc = source
         })
     } else {
@@ -99,53 +145,28 @@ function iframe(origin, parent) {
 
 
 
-window.addEventListener("resize", fit)
 window.addEventListener("load", function() {
-    var grid = document.getElementsByClassName("gh-grid")[0]
-    iframe("template/box3d.html", grid)
-    iframe("template/grid.html", grid)
-    iframe("template/hyperlink.html", grid)
-    iframe("template/playlist.html", grid)
-    // iframe("http://designtagebuch.de", grid)
-    // iframe("https://mirelleborra.com", grid)
-    iframe("https://freshman.tech/custom-html5-video/", grid)
-    // iframe("https://www.youtube.com/channel/UC3Qk1lecHOkzYqIqeqj8uyA?view_as=subscriber", grid)
-    // iframe("https://amazon.de", grid)
+    proxy("https://freshman.tech/custom-html5-video/", function(source) {
+        var frm = document.createElement("iframe")
+        frm.addEventListener("load", fit.bind(frm))
+        frm.src = "about:blank"
+        frm.srcdoc = source
+        document.body.appendChild(frm)
+    })
 })
 
 
 
-
-
-// try this:
-// copy(origin)
-// then append to html source <base href="https://api.allorigins.win/get?callback=update&url=" + iframe.location.origin>
-// the base should automatically prepend all href, action, src values
-// when user clicks link inside the iframe or fires an action, it will go through the proxy obove and come back
-// and invoke the update() callback function
-// which updates the iframe.srcdoc (which hopefully re-parses the dom tree automatically)
-
-// see https://developer.mozilla.org/de/docs/Web/HTML/Element/base
-// and 
-
-
-
-
-//https://developer.mozilla.org/en-US/docs/Web/API/Window/frames
-//If you have some logic based on the styles of the iframe tag in the parent page (e.g. its width or height), you can use window.frameElement which will point you to the containing iframe object:
-/*
-The most simple, efficient and correct way to do so it to just use URL api.
-
-new URL("http://www.stackoverflow.com?q=hello").href
-//=> http://www.stackoverflow.com/?q=hello"
-
-new URL("mypath","http://www.stackoverflow.com").href
-//=> "http://www.stackoverflow.com/mypath"
-
-new URL("../mypath","http://www.stackoverflow.com/search").href
-//=> "http://www.stackoverflow.com/mypath"
-
-new URL("../mypath", document.baseURI).href
-//=> "https://stackoverflow.com/questions/mypath"
-Performance wise, this solution is on par with using string manipulation and twice as fast as creating a tag.
-*/
+// window.addEventListener("resize", fit)
+// window.addEventListener("load", function() {
+//     var grid = document.getElementsByClassName("gh-grid")[0]
+//     iframe("template/box3d.html", grid)
+//     iframe("template/grid.html", grid)
+//     iframe("template/hyperlink.html", grid)
+//     iframe("template/playlist.html", grid)
+//     // iframe("http://designtagebuch.de", grid)
+//     // iframe("https://mirelleborra.com", grid)
+//     iframe("https://freshman.tech/custom-html5-video/", grid)
+//     // iframe("https://www.youtube.com/channel/UC3Qk1lecHOkzYqIqeqj8uyA?view_as=subscriber", grid)
+//     // iframe("https://amazon.de", grid)
+// })
