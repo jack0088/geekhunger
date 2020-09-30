2071c6a4457787a605ffec67dd592abde87de469
herrsch <jack0088@me.com>
2020-09-30 17:13:36 +0200

    rewrote  same-origin filter and then fixed a bug that was not considering iframes.srcdoc attribute as same-origin

--- gh/iframe.js
+++ gh/iframe.js
@@ -1,22 +1,28 @@
 function gh_fit() {
-    document.querySelectorAll("iframe.gh-fit").forEach(function(iframe) {
-        if(location.origin === new URL(iframe.src).origin) { // avoid collision with CORS restriction
-            var doc = iframe.contentWindow.document
-            var html = doc.documentElement
-            var body = doc.body
+    Array.from(document.querySelectorAll("iframe.gh-fit"))
+    .filter(function(iframe) { // same-origin
+        return typeof iframe.srcdoc === "string"
+        || typeof iframe.src !== "string"
+        || iframe.src.length < 1
+        || iframe.src === "about:blank"
+        || new URL(iframe.src).origin === location.origin
+    })
+    .forEach(function(iframe) {
+        var doc = iframe.contentWindow.document
+        var html = doc.documentElement
+        var body = doc.body
+
+        if(body) {
+            body.style.overflowX = "scroll" // scrollbar-jitter fix
+            body.style.overflowY = "hidden"
+        }
+        if(html) {
+            html.style.overflowX = "scroll" // scrollbar-jitter fix
+            html.style.overflowY = "hidden"
     
-            if(body) {
-                body.style.overflowX = "scroll" // scrollbar-jitter fix
-                body.style.overflowY = "hidden"
-            }
-            if(html) {
-                html.style.overflowX = "scroll" // scrollbar-jitter fix
-                html.style.overflowY = "hidden"
-        
-                var style = getComputedStyle(html)
-                iframe.width = parseInt(style.getPropertyValue("width")) // round value
-                iframe.height = parseInt(style.getPropertyValue("height"))
-            }
+            var style = getComputedStyle(html)
+            iframe.width = parseInt(style.getPropertyValue("width")) // round value
+            iframe.height = parseInt(style.getPropertyValue("height"))
         }
     })
     requestAnimationFrame(gh_fit)
