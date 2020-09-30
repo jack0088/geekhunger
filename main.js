22ecbea62a43efa7c43d05d891e90dd730b07039
herrsch <jack0088@me.com>
2020-09-30 02:41:32 +0200

    improved how fast user will see results from server (before some items were queued up via Promises. Now we return things as we receive them)

--- main.js
+++ main.js
@@ -13,10 +13,9 @@ function gh_script(url) {
         }
 
         var script = document.createElement("script")
+        script.addEventListener("load", resolve.bind(this, script))
         script.type = "text/javascript"
         script.src = url
-        script.addEventListener("load", resolve.bind(this, script))
-        
         document.head.appendChild(script)
     })
 }
@@ -57,22 +56,17 @@ function gh_relink(source) {
 
 
 
-function gh_iframe(origin, parent, setup) {
+function gh_iframe(origin, parent) {
     return new Promise(function(resolve) {
         var iframe = document.createElement("iframe")
+        iframe.addEventListener("load", resolve.bind(this, iframe))
         iframe.src = "about:blank"
         iframe.style.maxWidth = "100%"
         iframe.style.overflowX = "scroll"
         iframe.style.overflowY = "hidden"
-        iframe.style.border = "none"
-        iframe.style.display = "none"
+        // iframe.style.border = "none"
+        iframe.style.display = "block"
         iframe.classList.add("gh-fit") // this is the css-class that gh/iframe.js automatically reacts to
-
-        iframe.addEventListener("load", function(event) {
-            setup(event.target) // custom func to batch-add additional css-classes, etc
-            event.target.style.display = "block"
-            return resolve(event.target)
-        })
 
         if(origin.startsWith("http") && !origin.startsWith(window.location.origin)) {
             gh_copy(origin).then(function(source) {
@@ -81,7 +75,6 @@ function gh_iframe(origin, parent, setup) {
         } else {
             iframe.src = origin
         }
-
         (parent || document.body).appendChild(iframe) // leverage native DOMParser
     })
     // TODO this should work for cross-origin sources like gh_fit(), via css class selector
@@ -91,7 +84,9 @@ function gh_iframe(origin, parent, setup) {
 
 
 
-async function gh_run() {
+addEventListener("DOMContentLoaded", function() {
+    gh_script("gh/iframe.js").catch(console.warn)
+
     var grid = document.getElementsByClassName("gh-grid")[0]
 
     function settings(iframe) {
@@ -99,48 +94,43 @@ async function gh_run() {
         iframe.classList.add("gh-fullwidth")
     }
     
-    // gh_iframe("template/box3d.html", grid, settings)
+    // gh_iframe("template/box3d.html", grid).then(settings)
 
-    // gh_iframe("template/grid.html", grid, settings).then(function(f) {
+    // gh_iframe("template/grid.html", grid).then(function(f) {
+    //     settings()
     //     f.classList.add("gh-fullwidth") // for debugging (just testing)
     //     f.style.backgroundColor = "red"
     // })
 
-    // gh_iframe("https://freshman.tech/custom-html5-video/", grid, settings).then(function(f) {
+    // gh_iframe("https://freshman.tech/custom-html5-video/", grid).then(function(f) {
+    //     settings()
     //     f.classList.add("gh-grid-double")
     // })
 
-    gh_iframe("template/media.html", grid, settings)
-    // gh_iframe("template/media.html", grid, settings)
-    // gh_iframe("template/media.html", grid, settings)
-    // gh_iframe("template/media.html", grid, settings)
-    // gh_iframe("template/media.html", grid, settings)
-    // gh_iframe("template/media.html", grid, settings)
-    // gh_iframe("template/media.html", grid, settings)
-    // gh_iframe("template/media.html", grid, settings)
-    // gh_iframe("template/media.html", grid, settings)
-    // gh_iframe("template/media.html", grid, settings)
-    // gh_iframe("template/media.html", grid, settings)
-    // gh_iframe("template/media.html", grid, settings)
-    // gh_iframe("template/media.html", grid, settings)
-    // gh_iframe("template/media.html", grid, settings)
-    // gh_iframe("template/media.html", grid, settings)
-    // gh_iframe("template/media.html", grid, settings)
-    // gh_iframe("template/media.html", grid, settings)
-    // await gh_iframe("template/hyperlink.html", grid, settings)
+    gh_iframe("template/media.html", grid).then(settings)
+    // gh_iframe("template/media.html", grid).then(settings)
+    // gh_iframe("template/media.html", grid).then(settings)
+    // gh_iframe("template/media.html", grid).then(settings)
+    // gh_iframe("template/media.html", grid).then(settings)
+    // gh_iframe("template/media.html", grid).then(settings)
+    // gh_iframe("template/media.html", grid).then(settings)
+    // gh_iframe("template/media.html", grid).then(settings)
+    // gh_iframe("template/media.html", grid).then(settings)
+    // gh_iframe("template/media.html", grid).then(settings)
+    // gh_iframe("template/media.html", grid).then(settings)
+    // gh_iframe("template/media.html", grid).then(settings)
+    // gh_iframe("template/media.html", grid).then(settings)
+    // gh_iframe("template/media.html", grid).then(settings)
+    // gh_iframe("template/media.html", grid).then(settings)
+    // gh_iframe("template/media.html", grid).then(settings)
+    // gh_iframe("template/media.html", grid).then(settings)
+    // gh_iframe("template/hyperlink.html", grid).then(settings)
     
-    // gh_iframe("https://css-tricks.com/snippets/jquery/fit-iframe-to-content")
+    // gh_iframe("https://css-tricks.com/snippets/jquery/fit-iframe-to-content").then((e) => {e.classList.add("gh-fullwidth")})
 
-    // await gh_iframe("https://apple.com")
+    // gh_iframe("https://apple.com")
     // gh_iframe("http://designtagebuch.de")
     // gh_iframe("https://mirelleborra.com", grid)
     // gh_iframe("https://www.youtube.com/channel/UC3Qk1lecHOkzYqIqeqj8uyA?view_as=subscriber", grid)
     // gh_iframe("https://amazon.de", grid)
-}
-
-
-
-addEventListener("load", async function() {
-    await gh_script("gh/iframe.js").catch(console.warn)
-    gh_run().then(gh_fit) // invoke gh_fit manually, as the script will miss its load-event uppon inclusion
 })
