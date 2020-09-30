4226fd25d7456b6ee188f67273819f0856ca702b
herrsch <jack0088@me.com>
2020-09-30 03:24:37 +0200

    renamed av.* back to player and playlist; playlist.css includes hyperlink.css

---
+++ gh/player.js
@@ -0,0 +1,49 @@
+function gh_players(loop) {
+    document.querySelectorAll("video.gh-player, audio.gh-player").forEach(loop)
+}
+
+function gh_preset() {
+    gh_players(function(player) {
+        player.controls = false
+        player.autoplay = false
+        player.loop = false
+        player.muted = false
+        // Interesting fact about player.preload = "metadata"
+        // meta information is always fetched after(!) the source has been loaded
+        // browsers treat this setting almost identical to "auto"
+        // concluding, a value of "none" would never have access to source .duration
+
+        player.addEventListener("loadedmetadata", function(event) {
+            console.log("loadedmeta", event.target, event.target.currentTime)
+        })
+
+        player.addEventListener("durationchange", function(event) {
+            console.log("durationchange", event.target, event.target.currentTime)
+        })
+
+        player.addEventListener("loadeddata", function(event) {
+            console.log("dataloaded", event.target, event.target.currentTime)
+        })
+
+        player.addEventListener("canplay", function(event) {
+            console.log("canplay", event.target, event.target.currentTime)
+        })
+
+        player.addEventListener("loadstart", function(event) {
+            player.currentTime = 0.001 // request first-frame preview
+            console.log("loadstart!!!", event.target, event.target.currentTime)
+        })
+        
+    })
+}
+
+
+addEventListener("DOMContentLoaded", function() {
+    gh_preset()
+})
+
+addEventListener("load", function() {
+    gh_players(function(player) {
+        player.load()
+    })
+})
