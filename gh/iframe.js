19d787b33a5fa045e38e7b3ccfe05c8f97434cc0
herrsch <jack0088@me.com>
2020-09-30 05:04:08 +0200

    fix error - do not try make cross-origin iframe responsive as js cant access it

--- gh/iframe.js
+++ gh/iframe.js
@@ -1,20 +1,22 @@
 function gh_fit() {
     document.querySelectorAll("iframe.gh-fit").forEach(function(iframe) {
-        var doc = iframe.contentWindow.document
-        var html = doc.documentElement
-        var body = doc.body
-
-        if(body) {
-            body.style.overflowX = "scroll" // scrollbar-jitter fix
-            body.style.overflowY = "hidden"
-        }
-        if(html) {
-            html.style.overflowX = "scroll" // scrollbar-jitter fix
-            html.style.overflowY = "hidden"
+        if(location.origin === new URL(iframe.src).origin) { // avoid collision with CORS restriction
+            var doc = iframe.contentWindow.document
+            var html = doc.documentElement
+            var body = doc.body
     
-            var style = getComputedStyle(html)
-            iframe.width = parseInt(style.getPropertyValue("width")) // round value
-            iframe.height = parseInt(style.getPropertyValue("height"))
+            if(body) {
+                body.style.overflowX = "scroll" // scrollbar-jitter fix
+                body.style.overflowY = "hidden"
+            }
+            if(html) {
+                html.style.overflowX = "scroll" // scrollbar-jitter fix
+                html.style.overflowY = "hidden"
+        
+                var style = getComputedStyle(html)
+                iframe.width = parseInt(style.getPropertyValue("width")) // round value
+                iframe.height = parseInt(style.getPropertyValue("height"))
+            }
         }
     })
     requestAnimationFrame(gh_fit)
