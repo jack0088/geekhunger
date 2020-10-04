7265e25e167e16bc2243b359c0aeb5bcd0cbce4f
herrsch <jack0088@me.com>
2020-10-04 15:17:34 +0200

    cleaned up test code

--- main.js
+++ main.js
@@ -40,17 +40,8 @@ async function gh_copy(origin) {
 
 
 async function gh_relink(source) {
+    // TODO
     // var blacklist = page.querySelectorAll("[src], [href], [action]")
-    // console.log(page.URL)
-    // console.log(blacklist)
-    // for(var id = 0; id < blacklist.length; id++) {
-    //     var elem = blacklist[id]
-    //     if(elem.getAttribute("src")) elem.setAttribute("src", reset(elem.getAttribute("src")))
-    //     if(elem.getAttribute("href")) elem.setAttribute("href", reset(elem.getAttribute("href")))
-    //     if(elem.getAttribute("action")) elem.setAttribute("action", reset(elem.getAttribute("action")))
-    //     // url() in stylesheets?
-            // references in other files like .js? should I handle these as well?
-    // }
     return source
 }
 
@@ -58,10 +49,10 @@ async function gh_relink(source) {
 
 function gh_proxy(origin, iframe) {
     if(typeof origin !== "undefined" && typeof iframe !== "undefined") {
-        gh_copy(origin).then(function(copy) {
-            gh_relink(copy).then(function(source) {
-                iframe.srcdoc = source
-            })
+        gh_copy(origin)
+        .then(gh_relink)
+        .then(function(source) {
+            iframe.srcdoc = source
         })
     } else {
         Array.from(document.querySelectorAll("iframe.gh-fit"))
@@ -81,17 +72,12 @@ function gh_proxy(origin, iframe) {
 
 function gh_iframe(origin, parent) {
     var iframe = document.createElement("iframe")
+    // iframe.sandbox = "allow-same-origin allow-scripts" // disable sandbox
     iframe.classList.add("gh-fit") // this is the css-class that gh/iframe.js automatically reacts to
     iframe.classList.add("gh-fullwidth")
-    iframe.style.maxWidth = "100%"
-    iframe.style.overflowX = "scroll"
-    iframe.style.overflowY = "hidden"
-    iframe.style.border = "none"
-    iframe.style.display = "block"
-    // iframe.sandbox = "allow-same-origin allow-scripts" // disable sandbox
     document.appendChild.call(parent || document.body, iframe) // Why??? Because:
-    // I omit semicolons and js compiler screws up sometimes, with e.g. `TypeError "block" is not a function`
-    // Could have used `;(parent || document.body, iframe).appendChild(iframe)` instead
+    // I omit semicolons and this confuses the js compiler sometimes, e.g. `TypeError "block" is not a function`
+    // I could have used `;(parent || document.body, iframe).appendChild(iframe)` instead
     if(origin.startsWith("http") && !origin.startsWith(location.origin)) {
         iframe.src = "about:blank"
         gh_proxy(origin, iframe)
@@ -104,14 +90,8 @@ function gh_iframe(origin, parent) {
 
 
 addEventListener("DOMContentLoaded", function() {
-    
-    
     gh_script("gh/iframe.js").catch(console.warn)
     
-        document.addEventListener("readystatechange", (event) => {
-            console.log(document.readyState)
-        })
-
     var grid = document.getElementsByClassName("gh-grid")[0]
 
     function preset(iframe) {
@@ -121,53 +101,13 @@ addEventListener("DOMContentLoaded", function() {
     
     gh_iframe("template/media.html", grid)
 
-
-
-
-
-    // gh_iframe("template/grid.html", grid).then(function(f) {
-    //     preset()
-    //     f.classList.add("gh-fullwidth") // for debugging (just testing)
-    //     f.style.backgroundColor = "red"
-    // })
-
-    // gh_iframe("https://freshman.tech/custom-html5-video/", grid)
-    // gh_iframe("https://freshman.tech/custom-html5-video/", grid).then(function(f) {
-    //     preset()
-    //     f.classList.add("gh-grid-double")
-    // })
-
-    // preset(gh_iframe("template/media.html", grid))
-
-    // gh_iframe("template/media.html", grid).then(preset)
-    // gh_iframe("template/media.html", grid).then(preset)
-    // gh_iframe("template/media.html", grid).then(preset)
-    // gh_iframe("template/media.html", grid).then(preset)
-    // gh_iframe("template/media.html", grid).then(preset)
-    // gh_iframe("template/media.html", grid).then(preset)
-    // gh_iframe("template/media.html", grid).then(preset)
-    // gh_iframe("template/media.html", grid).then(preset)
-    // gh_iframe("template/media.html", grid).then(preset)
-    // gh_iframe("template/media.html", grid).then(preset)
-    // gh_iframe("template/media.html", grid).then(preset)
-    // gh_iframe("template/media.html", grid).then(preset)
-    // gh_iframe("template/media.html", grid).then(preset)
-    // gh_iframe("template/media.html", grid).then(preset)
-    // gh_iframe("template/media.html", grid).then(preset)
-    // gh_iframe("template/media.html", grid).then(preset)
-    // gh_iframe("template/hyperlink.html", grid).then(preset)
-    
-    // gh_iframe("https://css-tricks.com/snippets/jquery/fit-iframe-to-content").then((e) => {e.classList.add("gh-fullwidth")})
-    // var a = gh_iframe("https://css-tricks.com/snippets/jquery/fit-iframe-to-content")
-    // a.removeAttribute("src")
-    // a.classList.add("gh-fullwidth")
-
+    gh_iframe("https://css-tricks.com/snippets/jquery/fit-iframe-to-content")
+    // gh_iframe("https://freshman.tech/custom-html5-video", grid)
     // gh_iframe("https://apple.com")
     // gh_iframe("http://designtagebuch.de")
     // gh_iframe("https://mirelleborra.com", grid)
     // gh_iframe("https://www.youtube.com/channel/UC3Qk1lecHOkzYqIqeqj8uyA?view_as=subscriber", grid)
     // gh_iframe("https://amazon.de", grid)
 
-
-    gh_proxy()
+    // gh_proxy()
 })
